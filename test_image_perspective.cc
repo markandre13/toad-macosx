83136a4f279225458c8717ce697ab2363050ed76
Mark-Andre Hopf <mhopf@mark13.org>
2015-04-07 19:58:01 +0200

    experimental bézier transformation of a bitmap

--- test_image_perspective.cc
+++ test_image_perspective.cc
@@ -42,26 +42,12 @@ TPoint src[4] = {
   { 0, 297 }
 };
 
-TPoint dst[16] = {
-  { 300, 100 },
-  { 400, 100 },
-  { 500, 100 },
-  { 600, 100 },
-
-  { 300, 200 },
-  { 400, 200 },
-  { 500, 200 },
-  { 600, 200 },
-
-  { 300, 300 },
-  { 400, 300 },
-  { 500, 300 },
-  { 600, 300 },
-
-  { 300, 400 },
-  { 400, 400 },
-  { 500, 400 },
-  { 600, 400 }
+TPoint dst[5] = {
+  { 360, 20 },
+  { 360+375, 20 },
+  { 360+375, 20+297 },
+  { 360, 20+297 },
+  { 360+375/2, 20+297/2 },
 };
 
 class TMyWindow:
@@ -79,8 +65,7 @@ class TMyWindow:
     }
     
     void paint() override;
-    void clearBitmap();
-    void transformBitmap(const TPoint *dst, TCoord x=0.0, TCoord y=0.0, TCoord w=1.0, TCoord h=1.0);
+    void transformBitmap();
     void mouseEvent(const TMouseEvent&) override;
 };
 
@@ -90,7 +75,7 @@ TMyWindow::mouseEvent(const TMouseEvent &me)
   switch(me.type) {
     case TMouseEvent::LDOWN:
       handle=-1;
-      for(int i=0; i<16; ++i) {
+      for(int i=0; i<5; ++i) {
         if(dst[i].x-4<=me.pos.x && me.pos.x <= dst[i].x+4 &&
            dst[i].y-4<=me.pos.y && me.pos.y <= dst[i].y+4)
         {
@@ -220,18 +205,13 @@ format(const char *fmt, ...)
 }
 
 void
-TMyWindow::clearBitmap()
+TMyWindow::transformBitmap()
 {
   if (bdst.img) {
     unsigned char *data = [bdst.img bitmapData];
     memset(data, 0, 4* bdst.width* bdst.height);
   }
-}
 
-void
-TMyWindow::transformBitmap(const TPoint *dst, TCoord sx, TCoord sy, TCoord sw, TCoord sh)
-{
-  // find min and max of dst
   TPoint A(dst[0]);
   TPoint B(dst[1]);
   TPoint C(dst[2]);
@@ -244,7 +224,6 @@ TMyWindow::transformBitmap(const TPoint *dst, TCoord sx, TCoord sy, TCoord sw, T
     p1.x = max(p1.x, dst[i].x);
     p1.y = max(p1.y, dst[i].y);
   }
-  
   TPoint Q;
   for(Q.y=p0.y; Q.y<=p1.y; ++Q.y) {
     for(Q.x=p0.x; Q.x<=p1.x; ++Q.x) {
@@ -263,120 +242,29 @@ TMyWindow::transformBitmap(const TPoint *dst, TCoord sx, TCoord sy, TCoord sw, T
         continue;
       
       TCoord r,g,b;
-      bsrc.getPixel((sx+x*sw)*bsrc.width, (sy+y*sh)*bsrc.height, &r, &g ,&b);
+      bsrc.getPixel(x*bsrc.width, y*bsrc.height, &r, &g ,&b);
 //cout << a*174 << ", " << b*255 << endl;
 //      pen.setColor(r,g,b);
 //      pen.fillRectangle(Q.x, Q.y, 1,1);
       bdst.setPixel(Q.x, Q.y, r, g, b);
-      bdst.setPixel(Q.x+1, Q.y, r, g, b);
-      bdst.setPixel(Q.x+1, Q.y+1, r, g, b);
-      bdst.setPixel(Q.x, Q.y+1, r, g, b);
     }
   }
 }
-
-// Fakultät
-long fak(long n) {
-  long r=1;
-  for(long i=2; i<=n; ++i)
-    r*=i;
-  return r;
-}
-
-// Binomial Koeffizient
-TCoord C(long n, long i)
-{
-  return (TCoord)fak(n) / (TCoord)( fak(i) * fak(n-i) );
-}
-
-// Bernsteinpolynom
-TCoord B(long i, long n, TCoord t)
-{
-  return C(n, i) * pow(t, i) * pow(1-t, n-i);
-}
-
 
 void
 TMyWindow::paint()
 {
   if (bitmapDirty) {
-    clearBitmap();
-//    transformBitmap();
+    transformBitmap();
     bitmapDirty = false;
   }
 
   TPen pen(this);
   pen.translate(0.5, 0.5);
 
-  TCoord s = 0.05;
-  for(TCoord u=0.0; u<1.0; u+=s) { // x
-    for(TCoord v=0.0; v<1.0; v+=s) { // y
-//cout << "uv="<<u<<","<<v<<endl;
-      TPoint pt[4];
-      for(int i=0; i<4; ++i) {
-        for(int j=0; j<4; ++j) {
-          pt[0] += B(i, 3, u) * B(j, 3, v) * dst[i+j*4];
-          pt[1] += B(i, 3, u+s) * B(j, 3, v) * dst[i+j*4];
-          pt[2] += B(i, 3, u+s) * B(j, 3, v+s) * dst[i+j*4];
-          pt[3] += B(i, 3, u) * B(j, 3, v+s) * dst[i+j*4];
-        }
-      }
-      transformBitmap(pt, u, v, s, s);
-      // pen.fillRectangle(pt.x, pt.y,1,1);
-    }
-  }
   pen.drawBitmap(0,0,bdst);
   pen.drawBitmap(0,0,bsrc);
   
-  pen.setColor(1,1,1);
-  for(int i=0; i<16; ++i) {
-    pen.drawRectangle(dst[i].x-2, dst[i].y-2, 5, 5);
-  }
-  
-  pen.drawBezier(dst, 4);
-  pen.drawBezier(dst+12, 4);
-  pen.drawBezier(dst+12, 4);
-  pen.drawBezier(dst+12, 4);
-  TPoint foo[4];
-  foo[0]=dst[3];
-  foo[1]=dst[7];
-  foo[2]=dst[11];
-  foo[3]=dst[15];
-  pen.drawBezier(foo, 4);
-  foo[0]=dst[12];
-  foo[1]=dst[8];
-  foo[2]=dst[4];
-  foo[3]=dst[0];
-  pen.drawBezier(foo, 4);
-  
-  pen.setLineStyle(TPen::DOT);
-  pen.drawLine(dst[0], dst[1]);
-  pen.drawLine(dst[2], dst[3]);
-  
-  pen.drawLine(dst[3], dst[7]);
-  pen.drawLine(dst[11], dst[15]);
-  
-  pen.drawLine(dst[15], dst[14]);
-  pen.drawLine(dst[13], dst[12]);
-  
-  pen.drawLine(dst[12], dst[8]);
-  pen.drawLine(dst[4], dst[0]);
-
-  pen.drawBezier(dst+4, 4);
-  pen.drawBezier(dst+8, 4);
-
-/*
-  pen.setColor(1,0,0);
-  for(int x=300; x<=450; x+=10) {
-    for(int y=100; y<=250; y+=10) {
-      pen.fillRectangle(x,y,1,1);
-    }
-  }
-*/
-//cout<<"----------"<<endl;
-
-
-#if 0  
   TPoint A(dst[0]);
   TPoint B(dst[1]);
   TPoint C(dst[2]);
@@ -420,7 +308,6 @@ TMyWindow::paint()
   pen.drawLine(I,J);
 
   pen.drawString(10,200, format("a=%f, b=%f", a, b));
-#endif
 }
 
 } // unnamed namespace
