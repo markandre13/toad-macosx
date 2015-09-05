d8d3be91cd25856df920875a85497d7bd7e2a142
Mark-Andre Hopf <mhopf@mark13.org>
2015-09-05 23:40:55 +0200

    added more boolean op tests

--- toadtest.cc
+++ toadtest.cc
@@ -11,11 +11,7 @@ boolean(const TVectorPath &a, const TVectorPath &b, cbop::BooleanOpType op)
   return c;
 }
 
-TEST(MathTest, TwoPlusTwoEqualsFour) {
-  EXPECT_EQ(2 + 2, 4);
-}
-
-TEST(BooleanOp, Union) {
+TEST(BooleanOp, OverlapUnion) {
   TVectorPath p0;
   p0.move(TPoint(10,10));
   p0.line(TPoint(30,10));
@@ -40,6 +36,37 @@ TEST(BooleanOp, Union) {
   ex.line(TPoint(20,30));
   ex.line(TPoint(10,30));
   ex.close();
+
+  TVectorPath result = boolean(p0, p1, cbop::UNION);
+  
+  if (result==ex) {
+    SUCCEED();
+  } else {
+    FAIL() << "expected " << ex << "but got " << result;
+  }
+}
+
+TEST(BooleanOp, OutsideInsideUnion) {
+  TVectorPath p0;
+  p0.move(TPoint(10,10));
+  p0.line(TPoint(40,10));
+  p0.line(TPoint(40,40));
+  p0.line(TPoint(10,40));
+  p0.close();
+
+  TVectorPath p1;
+  p1.move(TPoint(20,20));
+  p1.line(TPoint(30,20));
+  p1.line(TPoint(30,30));
+  p1.line(TPoint(20,30));
+  p1.close();
+  
+  TVectorPath ex;
+  ex.move(TPoint(10,10));
+  ex.line(TPoint(40,10));
+  ex.line(TPoint(40,40));
+  ex.line(TPoint(10,40));
+  ex.close();
   
   TVectorPath result = boolean(p0, p1, cbop::UNION);
   
@@ -49,6 +76,214 @@ TEST(BooleanOp, Union) {
     FAIL() << "expected " << ex << "but got " << result;
   }
 }
+
+TEST(BooleanOp, OverlapIntersection) {
+  TVectorPath p0;
+  p0.move(TPoint(10,10));
+  p0.line(TPoint(30,10));
+  p0.line(TPoint(30,30));
+  p0.line(TPoint(10,30));
+  p0.close();
+
+  TVectorPath p1;
+  p1.move(TPoint(20,20));
+  p1.line(TPoint(40,20));
+  p1.line(TPoint(40,40));
+  p1.line(TPoint(20,40));
+  p1.close();
+  
+  TVectorPath ex;
+  ex.move(TPoint(20,20));
+  ex.line(TPoint(30,20));
+  ex.line(TPoint(30,30));
+  ex.line(TPoint(20,30));
+  ex.close();
+  
+  TVectorPath result = boolean(p0, p1, cbop::INTERSECTION);
+  
+  if (result==ex) {
+    SUCCEED();
+  } else {
+    FAIL() << "expected " << ex << "but got " << result;
+  }
+}
+
+TEST(BooleanOp, OutsideInsideIntersection) {
+  TVectorPath p0;
+  p0.move(TPoint(10,10));
+  p0.line(TPoint(40,10));
+  p0.line(TPoint(40,40));
+  p0.line(TPoint(10,40));
+  p0.close();
+
+  TVectorPath p1;
+  p1.move(TPoint(20,20));
+  p1.line(TPoint(30,20));
+  p1.line(TPoint(30,30));
+  p1.line(TPoint(20,30));
+  p1.close();
+  
+  TVectorPath ex;
+  ex.move(TPoint(20,20));
+  ex.line(TPoint(30,20));
+  ex.line(TPoint(30,30));
+  ex.line(TPoint(20,30));
+  ex.close();
+  
+  TVectorPath result = boolean(p0, p1, cbop::INTERSECTION);
+  
+  if (result==ex) {
+    SUCCEED();
+  } else {
+    FAIL() << "expected " << ex << "but got " << result;
+  }
+}
+
+TEST(BooleanOp, OverlapDifference) {
+  TVectorPath p0;
+  p0.move(TPoint(10,10));
+  p0.line(TPoint(30,10));
+  p0.line(TPoint(30,30));
+  p0.line(TPoint(10,30));
+  p0.close();
+
+  TVectorPath p1;
+  p1.move(TPoint(20,20));
+  p1.line(TPoint(40,20));
+  p1.line(TPoint(40,40));
+  p1.line(TPoint(20,40));
+  p1.close();
+  
+  TVectorPath ex;
+  ex.move(TPoint(10,10));
+  ex.line(TPoint(30,10));
+  ex.line(TPoint(30,20));
+  ex.line(TPoint(20,20));
+  ex.line(TPoint(20,30));
+  ex.line(TPoint(10,30));
+  ex.close();
+  
+  TVectorPath result = boolean(p0, p1, cbop::DIFFERENCE);
+  
+  if (result==ex) {
+    SUCCEED();
+  } else {
+    FAIL() << "expected " << ex << "but got " << result;
+  }
+}
+
+TEST(BooleanOp, OutsideInsideDifference) {
+  TVectorPath p0;
+  p0.move(TPoint(10,10));
+  p0.line(TPoint(40,10));
+  p0.line(TPoint(40,40));
+  p0.line(TPoint(10,40));
+  p0.close();
+
+  TVectorPath p1;
+  p1.move(TPoint(20,20));
+  p1.line(TPoint(30,20));
+  p1.line(TPoint(30,30));
+  p1.line(TPoint(20,30));
+  p1.close();
+  
+  TVectorPath ex;
+  ex.move(TPoint(10,10));
+  ex.line(TPoint(40,10));
+  ex.line(TPoint(40,40));
+  ex.line(TPoint(10,40));
+  ex.close();
+  ex.move(TPoint(20,20));
+  ex.line(TPoint(30,20));
+  ex.line(TPoint(30,30));
+  ex.line(TPoint(20,30));
+  ex.close();
+  
+  TVectorPath result = boolean(p0, p1, cbop::DIFFERENCE);
+  
+  if (result==ex) {
+    SUCCEED();
+  } else {
+    FAIL() << "expected " << ex << "but got " << result;
+  }
+}
+
+TEST(BooleanOp, OverlapXor) {
+  TVectorPath p0;
+  p0.move(TPoint(10,10));
+  p0.line(TPoint(30,10));
+  p0.line(TPoint(30,30));
+  p0.line(TPoint(10,30));
+  p0.close();
+
+  TVectorPath p1;
+  p1.move(TPoint(20,20));
+  p1.line(TPoint(40,20));
+  p1.line(TPoint(40,40));
+  p1.line(TPoint(20,40));
+  p1.close();
+  
+  TVectorPath ex;
+  ex.move(TPoint(10,10));
+  ex.line(TPoint(30,10));
+  ex.line(TPoint(30,20));
+  ex.line(TPoint(20,20));
+  ex.line(TPoint(20,30));
+  ex.line(TPoint(10,30));
+  ex.close();
+  ex.move(TPoint(20,30));
+  ex.line(TPoint(30,30));
+  ex.line(TPoint(30,20));
+  ex.line(TPoint(40,20));
+  ex.line(TPoint(40,40));
+  ex.line(TPoint(20,40));
+  ex.close();
+  
+  TVectorPath result = boolean(p0, p1, cbop::XOR);
+  
+  if (result==ex) {
+    SUCCEED();
+  } else {
+    FAIL() << "expected " << ex << "but got " << result;
+  }
+}
+
+TEST(BooleanOp, OutsideInsideXor) {
+  TVectorPath p0;
+  p0.move(TPoint(10,10));
+  p0.line(TPoint(40,10));
+  p0.line(TPoint(40,40));
+  p0.line(TPoint(10,40));
+  p0.close();
+
+  TVectorPath p1;
+  p1.move(TPoint(20,20));
+  p1.line(TPoint(30,20));
+  p1.line(TPoint(30,30));
+  p1.line(TPoint(20,30));
+  p1.close();
+  
+  TVectorPath ex;
+  ex.move(TPoint(10,10));
+  ex.line(TPoint(40,10));
+  ex.line(TPoint(40,40));
+  ex.line(TPoint(10,40));
+  ex.close();
+  ex.move(TPoint(20,20));
+  ex.line(TPoint(30,20));
+  ex.line(TPoint(30,30));
+  ex.line(TPoint(20,30));
+  ex.close();
+  
+  TVectorPath result = boolean(p0, p1, cbop::XOR);
+  
+  if (result==ex) {
+    SUCCEED();
+  } else {
+    FAIL() << "expected " << ex << "but got " << result;
+  }
+}
+
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest( &argc, argv );
