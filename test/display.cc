1530f4daeba4b82c06a5f2097a08d05e0f7a12eb
Mark-Andre Hopf <mhopf@mark13.org>
2017-02-21 15:58:14 +0100

    test/display.cc: better independance from screen's colorspace

--- test/display.cc
+++ test/display.cc
@@ -101,35 +101,86 @@ distance(NSUInteger c0, NSUInteger c1)
   return c0<c1 ? c1-c0 : c0-c1;
 } 
 
-static inline bool
-compareColor(const NSUInteger *c0, const NSUInteger *c1)
-{
-  return distance(c0[0], c1[0])<5 &&
-         distance(c0[1], c1[1])<5 &&
-         distance(c0[2], c1[2])<5;
+struct RGBA {
+  NSUInteger data[4];
+  bool operator<(const RGBA &rhs) const {
+    for (int i=0; i<3; ++i) {
+      if (data[i] < rhs.data[i])
+        return true;
+      if (data[i] > rhs.data[i])
+        return false;
+    }
+    return false;
+  }
+  bool operator!=(const RGBA &rhs) const {
+    for (int i=0; i<3; ++i) {
+      if (distance(data[i], rhs.data[i]) > 1)
+        return true;
+    }
+    return false;
+  }
+};
+inline ostream& operator<<(ostream &s, const RGBA& c) {
+  return s<<'('<<c.data[0] << ", " << c.data[1] << ", " << c.data[2] << ')';
 }
 
+/**
+ * Actual pixel values do depend on the display's colorspace.
+ *
+ * To be independent off the displays colorspace the algorithm only compares
+ * the pattern, independent of it's color. Tests should therefore avoid using
+ * a color only once.
+ */
 static void
 compareImageFile(const string &file0, const string &file1)
 {
   TBitmap bmp0, bmp1;
   bmp0.load(file0);
   bmp1.load(file1);
-  
+
+#if 0
   if (bmp0.getWidth() != bmp1.getWidth() ||
       bmp0.getHeight() != bmp1.getHeight() )
   {
-    FAIL() << "size mismatch between " << file0 << " and " << file1 << "." << endl;
+    FAIL() << "size mismatch between " 
+           << file0 << " (" << bmp0.getWidth() << ", " << bmp0.getHeight() << ") and "
+           << file1 << " (" << bmp1.getWidth() << ", " << bmp1.getHeight() << ")" << endl;
     return;
   }
-        
+#endif
+
+  map<RGBA, RGBA> colormap;
+
   for(size_t x=1; x<bmp0.getWidth()-1; ++x) {
     for(size_t y=1; y<bmp0.getHeight()-2; ++y) {
-      NSUInteger data0[4], data1[4];
-      [bmp0.img getPixel: data0 atX: x y: y];
-      [bmp1.img getPixel: data1 atX: x y: y];
-      if (!compareColor(data0, data1)) {
-        FAIL() << "pixel mismatch between " << file0 << " and " << file1 << " at " << x << ", " << y << ".";
+      RGBA c0, c1;
+      [bmp0.img getPixel: c0.data atX: x y: y];
+      [bmp1.img getPixel: c1.data atX: x y: y];
+      auto ptr = colormap.find(c0);
+      if (ptr==colormap.end()) {
+        colormap[c0] = c1;
+      } else
+      if (ptr->second != c1) {
+        cout << "---------- file: " << file0 << endl;
+        for(size_t y=0; y<20; ++y) {
+          for(size_t x=0; x<20; ++x) {
+            NSUInteger data[4];
+            [bmp0.img getPixel: data atX: x y: y];
+            cout << "["<<x<<","<<y<<"]("<< data[0] << ", " << data[1] << ", " << data[2] << ")\t";
+          }
+          cout << endl;
+        }
+        cout << "---------- file: " << file1 << endl;
+        for(size_t y=0; y<20; ++y) {
+          for(size_t x=0; x<20; ++x) {
+            NSUInteger data[4];
+            [bmp1.img getPixel: data atX: x y: y];
+            cout << "["<<x<<","<<y<<"]("<< data[0] << ", " << data[1] << ", " << data[2] << ")\t";
+          }
+          cout << endl;
+        }
+        FAIL() << "pixel mismatch between " << file0 << " and " << file1 << " at " << x << ", " << y
+               << c0 << "->" << c1 << ", expected " << ptr->second << endl;
       }
     }
   }
